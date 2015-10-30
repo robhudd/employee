10b91b76294c4e185041b26ee77431508ef7014a
robhudd <gogators54.rh@gmail.com>
2015-10-29 22:28:28 -0400

    Night 10/28

--- main.py
+++ main.py
@@ -16,45 +16,51 @@ print employee_db["Rob"]
 
 def show_all():
     print "show_all"
-    print employee_db.items()
+    for name in employee_db:
+        print "Name: " + name + " Age: " ,employee_db.get(name), "\n"
 
 def add_employee():
     print "add_employee"
-    name = raw_input("Enter New Employees Name: ")
-    age = raw_input("Enter New Employees Age: ")
+    name = raw_input("Enter New Employees Name: " + "\n")
+    age = raw_input("Enter New Employees Age:" + "\n")
     new_employee = {name : age}
     employee_db.update(new_employee)
+    print employee_db.items()
 
 def delete_employee():
     print "delete_employee"
-    name = raw_input("Enter Name of Employee You Would Like to Delete: ")
+    name = raw_input("Enter Name of Employee You Would Like to Delete:" + "\n")
     if name.title() in employee_db:
-        cert = raw_input("Are You Sure: Enter Y for Yes or N for No: ")
+        cert = raw_input("Are You Sure: Enter Y for Yes or N for No:" + "\n")
         if cert == "Y" or cert == "y":
             del employee_db[name.title()]
+            print employee_db.items()
+        elif cert == "N" or cert == "n":
+            print "You Selected No, Returning to Main Menu" + "\n"
         else:
-            print "You Selected No"
+            print "Incorrect Input, Please Try Again." + "\n"
+            return delete_employee()
     else:
-        print "Name is not in Datebase"
+        print "Name is not in Datebase, Returning to Main Menu" + "\n"
 
 def find_employee():
     print "find_employee"
-    name = raw_input("Enter Employee You Are Looking For: ")
+    name = raw_input("Enter Employee You Are Looking For:" + "\n")
     if name.title() in employee_db:
-        print "this"
+        print name.title(), employee_db.get(name.title())
     else:
-        print "Employee is not in database"
+        print "Employee is not in database, returning to main menu" + "\n"
 
 
 def exit():
-    print "Exiting"
+    print "Exiting \n"
 
 
-print "Welcome to Employee Database System"
+print "\n" + "Welcome to Employee Database System" + "\n"
 while True:
     def menu_options():
         print "1. Show All Employees" + "\n" + "2. Add New Employee" + "\n" + "3. Delete Existing Employee" + "\n" + "4. Find Employees" + "\n" + "5. Exit Employee Database";
-        selection = raw_input("Enter a number 1-5 ");
+        selection = raw_input("Enter a number 1-5 " + "\n");
         return selection;
 
     user_selection = int(menu_options())
